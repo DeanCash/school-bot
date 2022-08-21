3624625dfd494ef0aeac018d70b13f11f756fd3d
DeanCash <72015294+DeanCash@users.noreply.github.com>
2022-08-21 21:37:12 +0200

    changed prefix names

--- app/assets/data.py
+++ app/assets/data.py
@@ -10,11 +10,17 @@ colorama.init(autoreset=True)
 
 command_prefix = '!'
 
-pr_app = f"[{Fore.RED}APPLICATION{Fore.RESET}] >> "
-pr_bot = f"[{Fore.BLUE}BOT{Fore.RESET}] >> "
-pr_error = f"[{Fore.LIGHTRED_EX}ERROR{Fore.RESET}] >> "
-pr_event = f"[{Fore.CYAN}EVENT{Fore.RESET}] >> "
-pr_command = lambda x: f"[{Fore.YELLOW}COMMAND{Fore.RESET}] | {x} >> "
+log_app = f"[{Fore.RED}APPLICATION{Fore.RESET}] >> "
+log_bot = f"[{Fore.BLUE}BOT{Fore.RESET}] >> "
+log_error = f"[{Fore.LIGHTRED_EX}ERROR{Fore.RESET}] >> "
+log_illegal = f"{Fore.LIGHTRED_EX}[{Fore.RESET}ILLEGAL{Fore.LIGHTRED_EX}]{Fore.RESET} >> "
+pr_err = f"{Fore.LIGHTRED_EX}!{Fore.RESET}"
+log_event = f"[{Fore.CYAN}EVENT{Fore.RESET}] >> "
+log_owner = f"[{Fore.YELLOW}OWNER{Fore.RESET}] >> "
+log_command = lambda x: f"[{Fore.YELLOW}COMMAND{Fore.RESET}] | {x} >> "
+
+shorten = lambda s, l: s[:l] + ' ...' if len(s) > l else s
+log_message_len = 50
 
 token_file = os.path.join("app", "private", "secrets.json")
 with open(token_file, "r") as f:
@@ -30,7 +36,7 @@ def italic(msg: str) -> str:
 def strike(msg: str) -> str:
     return f"__{msg}__"
 
-def code(msg: str) -> str:
+def quote(msg: str) -> str:
     return f"`{msg}`"
 
 def to_mention(user_id: int) -> str:
